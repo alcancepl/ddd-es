9993fe31b1bae8e9f07ef2666d62669ddd0b6094
Kristijan Deberni <kdeberni@rcs-pol.eu>
2016-06-06 11:18:42 +0200

    chore(nuget): prepare all nugets

--- src/DddAzure/Properties/AssemblyInfo.cs
+++ src/DddAzure/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DddAzure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Implementation of Ddd intefaces on Azure storage using streastone event sourcing")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("DddAzure")]
 [assembly: AssemblyCulture("")]
